ebd3ac68c8c8f9225975d43ab659a9d0eb216758
Isicheri <tech2dom@gmail.com>
2025-02-05 02:24:04 +0100

    creating the class for the block

---
+++ src/block/block.ts
@@ -0,0 +1,16 @@
+
+export class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public timestamp: number;
+    public data: string;
+
+    constructor() {
+        this.index 
+        this.hash
+        this.previousHash
+        this.timestamp
+        this.data
+    }
+}
